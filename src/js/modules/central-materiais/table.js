51425c36605fc05bac675b334844692215eeed38
radarinternet <desenvolvimento.radarinternet@gmail.com>
2026-06-02 10:23:08 -0300

    feat(central-materiais): remove item 'Vincular a curso' do menu de ações
    Removed the 'Vincular a curso' button from the table.

--- src/js/modules/central-materiais/table.js
+++ src/js/modules/central-materiais/table.js
@@ -223,10 +223,6 @@ var MatTable = (() => {
               <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
               Editar
             </button>
-            <button onclick="MatMod.abrirVincular('${m.id}');MatMod._closeMenus()">
-              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
-              Vincular a curso
-            </button>
             <button onclick="MatMod.duplicar('${m.id}');MatMod._closeMenus()">
               <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="8" y="8" width="13" height="13" rx="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>
               Duplicar
